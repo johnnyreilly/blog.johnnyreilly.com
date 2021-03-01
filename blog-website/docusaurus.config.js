1720fe34c39380271bd81f138c2ac755a60b646a
John Reilly <johnny_reilly@hotmail.com>
2021-03-01 06:35:23 +0000

    add docusauras

---
+++ blog-website/docusaurus.config.js
@@ -0,0 +1,108 @@
+module.exports = {
+  title: 'I CAN MAKE THIS WORK',
+  tagline: 'The tagline of my site',
+  url: 'https://blog.johnnyreilly.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'johnnyreilly', // Usually your GitHub org/user name.
+  projectName: 'blog.johnnyreilly.com', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'I CAN MAKE THIS WORK',
+      logo: {
+        alt: 'I CAN MAKE THIS WORK',
+        src: 'img/profile.jpg',
+      },
+      items: [
+        /*
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        */
+        {to: 'blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://github.com/facebook/docusaurus',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Style Guide',
+              to: 'docs/',
+            },
+            {
+              label: 'Second Doc',
+              to: 'docs/doc2/',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: false /*{
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        }*/,
+        blog: {
+          path: './blog',
+          routeBasePath: '/', // Set this value to '/'.
+          showReadingTime: true,
+          editUrl:
+            'https://github.com/johnnyreilly/blog.johnnyreilly.com/edit/main/blog-website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
--- blogger-backup/index.ts
+++ blogger-backup/index.ts
@@ -2,17 +2,24 @@ import fs from 'fs'
 
 // make JSON with npx fast-xml-parser blog-02-28-2021.xml -o blog.json
 
+async function fromJsonToMarkDown() {
+    const posts = await getPosts();
+    posts.forEach(post => console.log(post.link ?? post.title))
+    console.log('posts.length', posts.length)
+}
+
+fromJsonToMarkDown();
+
 interface Post { title: string; content: string; published: string; link: string }
 
-async function fromJsonToMarkDown() {
+async function getPosts(): Promise<Post[]> {
     const json = await fs.promises.readFile('./blog.json', 'utf8');
 
     const blog = JSON.parse(json);
 
     const postsRaw = blog.feed.entry
-        .filter((entry: any) =>
-            (entry.category['@_term'] === "http://schemas.google.com/blogger/2008/kind#post" ||
-                (Array.isArray(entry.category) && entry.category.some((category: any) => category['@_term'] === "http://schemas.google.com/blogger/2008/kind#post"))) &&
+        .filter((entry: any) => (entry.category['@_term'] === "http://schemas.google.com/blogger/2008/kind#post" ||
+            (Array.isArray(entry.category) && entry.category.some((category: any) => category['@_term'] === "http://schemas.google.com/blogger/2008/kind#post"))) &&
             entry?.control?.draft !== 'yes');
     const posts: Post[] = postsRaw.map((entry: any) => {
         return {
@@ -22,11 +29,8 @@ async function fromJsonToMarkDown() {
             link: entry.link.find((link: any) => link["@_type"] === "text/html")
                 ? entry.link.find((link: any) => link["@_type"] === "text/html")["@_href"]
                 : undefined
-        }
+        };
     });
-    posts.forEach(post => console.log(post.link ?? post.title))
-    console.log('posts.length', posts.length)
+    return posts;
 }
-
-fromJsonToMarkDown();
 
