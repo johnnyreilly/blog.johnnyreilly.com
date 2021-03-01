845eac115f118074d9dbbb0c11a6089d5be4fd00
John Reilly <johnny_reilly@hotmail.com>
2021-03-01 05:35:17 +0000

    get what we need

--- blogger-backup/index.ts
+++ blogger-backup/index.ts
@@ -2,24 +2,31 @@ import fs from 'fs'
 
 // make JSON with npx fast-xml-parser blog-02-28-2021.xml -o blog.json
 
-async function boot() {
+interface Post { title: string; content: string; published: string; link: string }
+
+async function fromJsonToMarkDown() {
     const json = await fs.promises.readFile('./blog.json', 'utf8');
 
     const blog = JSON.parse(json);
 
-    console.log('blog.feed.entry.length', blog.feed.entry.length)
-    console.log('published', blog.feed.entry.filter((entry: any) => entry.published).length)
-    const posts = blog.feed.entry
+    const postsRaw = blog.feed.entry
         .filter((entry: any) =>
-            entry.category['@_term'] === "http://schemas.google.com/blogger/2008/kind#post" ||
-            (Array.isArray(entry.category) && entry.category.some((category: any) => category['@_term'] === "http://schemas.google.com/blogger/2008/kind#post")))
-        .filter((entry: any) => entry?.control?.draft !== 'yes');
-    posts.forEach((entry: any) => {
-        console.log(entry.title['#text'])
+            (entry.category['@_term'] === "http://schemas.google.com/blogger/2008/kind#post" ||
+                (Array.isArray(entry.category) && entry.category.some((category: any) => category['@_term'] === "http://schemas.google.com/blogger/2008/kind#post"))) &&
+            entry?.control?.draft !== 'yes');
+    const posts: Post[] = postsRaw.map((entry: any) => {
+        return {
+            title: entry.title['#text'],
+            content: entry.content['#text'],
+            published: entry.published,
+            link: entry.link.find((link: any) => link["@_type"] === "text/html")
+                ? entry.link.find((link: any) => link["@_type"] === "text/html")["@_href"]
+                : undefined
+        }
     });
+    posts.forEach(post => console.log(post.link ?? post.title))
     console.log('posts.length', posts.length)
-
 }
 
-boot();
+fromJsonToMarkDown();
 
