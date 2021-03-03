55990dab2b95a27eccc2bc818dd7faee7d41dcb5
John Reilly <johnny_reilly@hotmail.com>
2021-03-03 06:13:01 +0000

    download images

--- blogger-backup/index.ts
+++ blogger-backup/index.ts
@@ -1,17 +1,22 @@
 import fs from 'fs'
+import path from 'path'
 import showdown from 'showdown';
 import he from 'he'
 import jsdom from 'jsdom'
+import axios from 'axios';
 
 // make JSON with npx fast-xml-parser blog-02-28-2021.xml -o blog.json
 
 async function fromJsonToMarkDown() {
     const posts = await getPosts();
-    posts.forEach(post => console.log(post.link ?? post.title, post.tags))
+    // posts.forEach(post => console.log(post.link ?? post.title, post.tags))
     console.log('posts.length', posts.length)
 
     const post = posts[1];
+    await makePostIntoContent(post);
+}
 
+async function makePostIntoContent(post: Post) {
     const converter = new showdown.Converter({});
     const filename = post.link
         .replace('http://blog.johnnyreilly.com/', '')
@@ -20,23 +25,36 @@ async function fromJsonToMarkDown() {
 
     const contentProcessed = he.decode(post.content)
         .replace(/<br \/>/g, '\n')
-    /*
-    <div class="separator" style="clear: both;"><a href="https://1.bp.blogspot.com/-UwrtZigWg78/YDqN82KbjVI/AAAAAAAAZTE/Umezr1MGQicnxMMr5rQHD4xKINg9fasDACLcBGAsYHQ/s783/traffic-to-app-service.png" style="display: block; padding: 1em 0; text-align: center; "><img alt="traffic to app service" border="0" width="600" data-original-height="753" data-original-width="783" src="https://1.bp.blogspot.com/-UwrtZigWg78/YDqN82KbjVI/AAAAAAAAZTE/Umezr1MGQicnxMMr5rQHD4xKINg9fasDACLcBGAsYHQ/s600/traffic-to-app-service.png"></a></div>
-     */
 
-    const images = [];
+    const images: string[] = [];
     const dom = new jsdom.JSDOM(contentProcessed);
-    const markdown = converter.makeMarkdown(contentProcessed, dom.window.document)
+    let markdown = converter.makeMarkdown(contentProcessed, dom.window.document)
         .replace(/#### /g, '## ')
+        /*
+        <div class="separator" style="clear: both;"><a href="https://1.bp.blogspot.com/-UwrtZigWg78/YDqN82KbjVI/AAAAAAAAZTE/Umezr1MGQicnxMMr5rQHD4xKINg9fasDACLcBGAsYHQ/s783/traffic-to-app-service.png" style="display: block; padding: 1em 0; text-align: center; "><img alt="traffic to app service" border="0" width="600" data-original-height="753" data-original-width="783" src="https://1.bp.blogspot.com/-UwrtZigWg78/YDqN82KbjVI/AAAAAAAAZTE/Umezr1MGQicnxMMr5rQHD4xKINg9fasDACLcBGAsYHQ/s600/traffic-to-app-service.png"></a></div>
+         */
         .replace(/<div.*(<img.*">).*<\/div>/g, (replacer) => {
             const div = new jsdom.JSDOM(replacer);
             const img = div?.window?.document?.querySelector("img");
-            const alt = img?.getAttribute('alt');
-            const src = img?.getAttribute('src');
-            images.push(src);
-            return `![${alt ?? ''}](${src})`
+            const alt = img?.getAttribute('alt') ?? '';
+            const src = img?.getAttribute('src') ?? '';
+
+            if (src) images.push(src);
+
+            return `![${alt}](${src})`
         })
         ;
+
+    const directory = filename.replace('.md', '');
+    for (const url of images) {
+        try {
+            const localUrl = await downloadImage(url, directory);
+            markdown = markdown.replace(url, '../static/blog/' + localUrl);
+        } catch (e) {
+            console.error(`Failed to download ${url}`, e)
+        }
+    }
+
     console.log(markdown)
 
 
@@ -51,10 +69,36 @@ hide_table_of_contents: false
 ${markdown}
 `;
 
-    await fs.promises.writeFile(`../blog-website/blog/${filename}`, content)
+    await fs.promises.writeFile(`../blog-website/blog/${filename}`, content);
 }
 
-fromJsonToMarkDown();
+async function downloadImage(url: string, directory: string) {
+    console.log(`Downloading ${url}`);
+    const pathParts = new URL(url).pathname.split('/');
+    const filename = pathParts[pathParts.length - 1];
+    const directoryTo = path.resolve('..', 'blog-website', 'static', 'blog', directory);
+    const pathTo = path.resolve('..', 'blog-website', 'static', 'blog', directory, filename);
+
+    if (!fs.existsSync(directoryTo)){
+        fs.mkdirSync(directoryTo);
+    }
+
+    const writer = fs.createWriteStream(pathTo)
+
+    const response = await axios({
+        url,
+        method: 'GET',
+        responseType: 'stream'
+    })
+
+    response.data.pipe(writer)
+
+    return new Promise<string>((resolve, reject) => {
+        writer.on('finish', () => resolve(directory + '/' +filename))
+        writer.on('error', reject)
+    })
+}
+
 
 interface Post { title: string; content: string; published: string; link: string; tags: string[]; }
 
@@ -108,3 +152,5 @@ image: https://i.imgur.com/mErPwqL.png
 hide_table_of_contents: false
 ---
 */
+
+fromJsonToMarkDown();
