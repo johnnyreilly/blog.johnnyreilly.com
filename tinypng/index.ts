ccf1fe47c9d9be048ed6b5ac667c2511ee5a3a3b
johnnyreilly <johnny_reilly@hotmail.com>
2022-10-04 15:14:13 +0100

    feat: start to use webp

--- tinypng/index.ts
+++ tinypng/index.ts
@@ -15,12 +15,20 @@ function setUpTinify() {
 
 const docusaurusDirectory = '../blog-website';
 
-async function getImageFiles() {
+async function getBlogDirsOrderedDescending() {
   const blogDirs = (
     await fs.promises.readdir(path.resolve(docusaurusDirectory, 'blog'))
   ).filter((file) =>
     fs.statSync(path.resolve(docusaurusDirectory, 'blog', file)).isDirectory()
   );
+
+  blogDirs.sort().reverse();
+
+  return blogDirs;
+}
+
+async function getImageFiles() {
+  const blogDirs = await getBlogDirsOrderedDescending();
 
   const imageFiles = blogDirs.flatMap((dir) => getImageFilesFromDirectory(dir));
 
@@ -36,7 +44,7 @@ function getImageFilesFromDirectory(dir: string) {
       (file) =>
         file.endsWith('.jpg') ||
         file.endsWith('.jpeg') ||
-        // file.endsWith('.gif') ||
+        file.endsWith('.webp') ||
         file.endsWith('.png')
     )
     .map((file) => path.resolve(docusaurusDirectory, 'blog', dir, file))
@@ -48,19 +56,42 @@ async function sleep(millis: number) {
 }
 
 async function processImageFiles(imageFiles: string[]) {
-  console.log(`Found ${imageFiles.length} image files`);
   let processed = 0;
   let failed: string[] = [];
 
-  for (const file of imageFiles) {
+  for (const imageFilePath of imageFiles) {
     try {
       console.log(`
-- Processing ${file}`);
-      tinify.fromFile(file).toFile(file);
+- Processing ${imageFilePath}`);
+      const originalImageFilePrefix = imageFilePath.substring(
+        0,
+        imageFilePath.lastIndexOf('.')
+      );
+      const originalImageFileExtension = imageFilePath.substring(
+        imageFilePath.lastIndexOf('.') + 1
+      );
+
+      const source = tinify.fromFile(imageFilePath);
+      const converted = source.convert({ type: ['image/webp', 'image/png'] });
+      const convertedExtension = await converted.result().extension();
+      const newImageFileName = `${originalImageFilePrefix}.${convertedExtension}`;
+      converted.toFile(newImageFileName);
+
+      const imageFileName = path.basename(imageFilePath);
+      if (imageFileName !== newImageFileName) {
+        await updateBlogPostImageReferences({
+          originalImageFileExtension,
+          convertedExtension,
+          imageFilePath,
+          imageFileName,
+          newImageFileName,
+        });
+      }
+
       console.log(`✅ Processed! (${++processed} of ${imageFiles.length})`);
     } catch (e) {
-      console.log(`❌ Failed to process ${file}`);
-      failed.push(file);
+      console.log(`❌ Failed to process ${imageFilePath}`);
+      failed.push(imageFilePath);
     }
     await sleep(1000);
   }
@@ -70,21 +101,56 @@ async function processImageFiles(imageFiles: string[]) {
   console.log('Done!');
 }
 
+async function updateBlogPostImageReferences({
+  originalImageFileExtension,
+  convertedExtension,
+  imageFilePath,
+  imageFileName,
+  newImageFileName,
+}: {
+  originalImageFileExtension: string;
+  convertedExtension: string | void;
+  imageFilePath: string;
+  imageFileName: string;
+  newImageFileName: string;
+}) {
+  console.log(
+    `File converted from ${originalImageFileExtension} to ${convertedExtension} - will update blog post`
+  );
+  const directory = path.dirname(imageFilePath);
+
+  const indexMdPath = path.join(directory, 'index.md');
+  const blogPostContent = await fs.promises.readFile(indexMdPath, 'utf-8');
+
+  await fs.promises.writeFile(
+    indexMdPath,
+    blogPostContent.replaceAll(imageFileName, newImageFileName)
+  );
+  console.log(`Blog post updated: ${indexMdPath}`);
+
+  await fs.promises.unlink(imageFilePath);
+  console.log(`Deleted original image: ${imageFilePath}`);
+}
+
 async function run() {
   setUpTinify();
 
-  const blogDir = process.env.BLOG_DIR;
+  let directory = '';
 
-  if (!blogDir) {
-    console.log(
-      'Run with: TINIFY_KEY=$YOUR_API_KEY BLOG_DIR=$YOUR_BLOG_DIRECTORY yarn start'
-    );
-    process.exit(1);
+  if (process.env.BLOG_DIR) {
+    directory = process.env.BLOG_DIR;
+  } else {
+    const blogDirs = await getBlogDirsOrderedDescending();
+    if (blogDirs.length === 0) {
+      console.log(`No blog directories found`);
+      process.exit(1);
+    } else {
+      directory = blogDirs[0];
+    }
   }
 
-  const imageFiles = await getImageFilesFromDirectory(blogDir);
-  // const imageFiles = await getImageFiles();
-
+  const imageFiles = await getImageFilesFromDirectory(directory);
+  console.log(`Found ${imageFiles.length} image files in ${directory}`);
   await processImageFiles(imageFiles);
 }
 
