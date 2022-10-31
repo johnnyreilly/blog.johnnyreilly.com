933bf2971058cecf059ba24e419554f5ba697909
John Reilly <johnny_reilly@hotmail.com>
2022-10-31 07:31:26 +0000

    fix: font url (#319)
    * fix: font url
    
    * feat: back to plugin
    
    * fix: stuff
    
    * fix

--- blog-website/docusaurus.config.js
+++ blog-website/docusaurus.config.js
@@ -1,3 +1,5 @@
+const FontPreloadPlugin = require('webpack-font-preload-plugin');
+
 //@ts-check
 const urlRegex = /^\/\d{4}\/\d{2}\/\d{2}\//;
 
@@ -78,28 +80,6 @@ const config = {
   ],
 
   headTags: [
-    // <link rel="preload" href="/fonts/Poppins-Regular.ttf" as="font" type="font/ttf" crossorigin="anonymous">
-    {
-      tagName: 'link',
-      attributes: {
-        rel: 'preload',
-        href: '/fonts/Poppins-Regular.ttf',
-        as: 'font',
-        type: 'font/ttf',
-        crossorigin: 'anonymous',
-      },
-    },
-    // <link rel="preload" href="/fonts/Poppins-Bold.ttf" as="font" type="font/ttf" crossorigin="anonymous">
-    {
-      tagName: 'link',
-      attributes: {
-        rel: 'preload',
-        href: '/fonts/Poppins-Bold.ttf',
-        as: 'font',
-        type: 'font/ttf',
-        crossorigin: 'anonymous',
-      },
-    },
     {
       tagName: 'link',
       attributes: {
@@ -110,6 +90,17 @@ const config = {
   ],
 
   plugins: [
+    function preloadFontPlugin(_context, _options) {
+      return {
+        name: 'preload-font-plugin',
+        configureWebpack(_config, _isServer) {
+          return {
+            plugins: [new FontPreloadPlugin()],
+          };
+        },
+      };
+    },
+
     function fontainePlugin(_context, _options) {
       return {
         name: 'fontaine-plugin',
