00bf3ef86a37682384d4a573be91a76ff70a37ac
johnnyreilly <johnny_reilly@hotmail.com>
2022-11-21 05:48:51 +0000

    format

--- trim-xml/index.ts
+++ trim-xml/index.ts
@@ -19,7 +19,9 @@ async function trimXML() {
   console.log(`Loading ${sitemapPath}`);
   const sitemapXml = await fs.promises.readFile(sitemapPath, 'utf8');
 
-  const parser = new XMLParser();
+  const parser = new XMLParser({
+    ignoreAttributes: false,
+  });
   let sitemap: Sitemap = parser.parse(sitemapXml);
 
   const rootUrl = 'https://blog.johnnyreilly.com';
@@ -35,13 +37,8 @@ async function trimXML() {
 
   sitemap.urlset.url = filteredUrls;
 
-  const builder = new XMLBuilder({ format: false });
-  const shorterSitemapXml = builder
-    .build(sitemap)
-    .replace(
-      '<?xml></?xml><urlset>',
-      '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">'
-    );
+  const builder = new XMLBuilder({ format: true, ignoreAttributes: false });
+  const shorterSitemapXml = builder.build(sitemap);
 
   console.log(`Saving ${sitemapPath}`);
   await fs.promises.writeFile(sitemapPath, shorterSitemapXml);
