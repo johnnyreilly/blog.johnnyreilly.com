bdd376a1b92a016a238801396f55371c6363b53f
johnnyreilly <johnny_reilly@hotmail.com>
2022-11-24 20:21:09 +0000

    feat: initial implementation

--- trim-xml/index.ts
+++ trim-xml/index.ts
@@ -1,10 +1,36 @@
 import { XMLParser, XMLBuilder } from 'fast-xml-parser';
 import fs from 'fs';
 import path from 'path';
+import { simpleGit, SimpleGit, SimpleGitOptions } from 'simple-git';
+
+const dateBlogUrlRegEx = /(\d\d\d\d\/\d\d\/\d\d)\/(.+)/;
+const rootUrl = 'https://blog.johnnyreilly.com';
+
+function getSimpleGit(): SimpleGit {
+  const baseDir = path.resolve(process.cwd(), '..');
+
+  const options: Partial<SimpleGitOptions> = {
+    baseDir,
+    binary: 'git',
+    maxConcurrentProcesses: 6,
+    trimmed: false,
+  };
+
+  const git = simpleGit(options);
+
+  return git;
+}
+
+interface SitemapUrl {
+  loc: string;
+  changefreq: string;
+  priority: number;
+  lastmod?: string;
+}
 
 interface Sitemap {
   urlset: {
-    url: { loc: string; changefreq: string; priority: number }[];
+    url: SitemapUrl[];
   };
 }
 
@@ -24,7 +50,6 @@ async function trimXML() {
   });
   let sitemap: Sitemap = parser.parse(sitemapXml);
 
-  const rootUrl = 'https://blog.johnnyreilly.com';
   const filteredUrls = sitemap.urlset.url.filter(
     (url) =>
       url.loc !== `${rootUrl}/tags` &&
@@ -36,9 +61,49 @@ async function trimXML() {
     `Reducing ${sitemap.urlset.url.length} urls to ${filteredUrls.length} urls`
   );
 
-  sitemap.urlset.url = filteredUrls;
+  const git = getSimpleGit();
 
-  const builder = new XMLBuilder({ format: false, ignoreAttributes: false });
+  const urls: SitemapUrl[] = [];
+  for (const url of filteredUrls) {
+    if (urls.includes(url)) {
+      continue;
+    }
+
+    try {
+      const pathWithoutRootUrl = url.loc.replace(rootUrl + '/', ''); // eg 2012/01/07/standing-on-shoulders-of-giants
+
+      const match = pathWithoutRootUrl.match(dateBlogUrlRegEx);
+
+      if (!match || !match[1] || !match[2]) {
+        urls.push(url);
+        continue;
+      }
+
+      const date = match[1].replaceAll('/', '-'); // eg 2012-01-07
+      const slug = match[2]; // eg standing-on-shoulders-of-giants
+
+      // const blogPostFrom2020OrBefore = date.substring(0, 4) <= '2020';
+      // if (blogPostFrom2020OrBefore) {
+      //   urls.push({ ...url, lastmod: date });
+      //   continue;
+      // }
+
+      const file = `blog-website/blog/${date}-${slug}/index.md`;
+      const log = await git.log({
+        file,
+      });
+
+      const lastmod = log.latest?.date.substring(0, 10);
+      urls.push(lastmod ? { ...url, lastmod } : url);
+      console.log(url.loc, lastmod);
+    } catch (e) {
+      console.log('file date not looked up', url.loc, e);
+      urls.push(url);
+    }
+  }
+  sitemap.urlset.url = urls;
+
+  const builder = new XMLBuilder({ format: true, ignoreAttributes: false });
   const shorterSitemapXml = builder.build(sitemap);
 
   console.log(`Saving ${sitemapPath}`);
