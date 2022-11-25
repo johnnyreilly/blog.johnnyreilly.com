cd0b1857830fe42da67d23fdcfb18382f8bc7152
John Reilly <johnny_reilly@hotmail.com>
2022-11-25 08:45:41 +0000

    Lastmod (#350)
    * feat: initial implementation
    
    * refactor
    
    * ug
    
    * try again
    
    * fetch depth
    
    * weird
    
    * initial post
    
    * title image
    
    * whoops

--- trim-xml/index.ts
+++ trim-xml/index.ts
@@ -1,11 +1,80 @@
 import { XMLParser, XMLBuilder } from 'fast-xml-parser';
 import fs from 'fs';
 import path from 'path';
+import { simpleGit, SimpleGit, SimpleGitOptions } from 'simple-git';
+
+const rootUrl = 'https://blog.johnnyreilly.com';
+
+interface SitemapUrl {
+  loc: string;
+  changefreq: string;
+  priority: number;
+  lastmod?: string;
+}
 
 interface Sitemap {
   urlset: {
-    url: { loc: string; changefreq: string; priority: number }[];
+    url: SitemapUrl[];
   };
+}
+
+function getSimpleGit(): SimpleGit {
+  const baseDir = path.resolve(process.cwd(), '..');
+
+  const options: Partial<SimpleGitOptions> = {
+    baseDir,
+    binary: 'git',
+    maxConcurrentProcesses: 6,
+    trimmed: false,
+  };
+
+  const git = simpleGit(options);
+
+  return git;
+}
+
+const dateBlogUrlRegEx = /(\d\d\d\d\/\d\d\/\d\d)\/(.+)/;
+
+async function enrichUrlsWithLastmod(
+  filteredUrls: SitemapUrl[]
+): Promise<SitemapUrl[]> {
+  const git = getSimpleGit();
+
+  const urls: SitemapUrl[] = [];
+  for (const url of filteredUrls) {
+    if (urls.includes(url)) {
+      continue;
+    }
+
+    try {
+      // example url.loc: https://blog.johnnyreilly.com/2012/01/07/standing-on-shoulders-of-giants
+      const pathWithoutRootUrl = url.loc.replace(rootUrl + '/', ''); // eg 2012/01/07/standing-on-shoulders-of-giants
+
+      const match = pathWithoutRootUrl.match(dateBlogUrlRegEx);
+
+      if (!match || !match[1] || !match[2]) {
+        urls.push(url);
+        console.log('failed to match', pathWithoutRootUrl, match);
+        continue;
+      }
+
+      const date = match[1].replaceAll('/', '-'); // eg 2012-01-07
+      const slug = match[2]; // eg standing-on-shoulders-of-giants
+
+      const file = `blog-website/blog/${date}-${slug}/index.md`;
+      const log = await git.log({
+        file,
+      });
+
+      const lastmod = log.latest?.date.substring(0, 10);
+      urls.push(lastmod ? { ...url, lastmod } : url);
+      console.log(url.loc, lastmod);
+    } catch (e) {
+      console.log('file date not looked up', url.loc, e);
+      urls.push(url);
+    }
+  }
+  return urls;
 }
 
 async function trimXML() {
@@ -24,7 +93,6 @@ async function trimXML() {
   });
   let sitemap: Sitemap = parser.parse(sitemapXml);
 
-  const rootUrl = 'https://blog.johnnyreilly.com';
   const filteredUrls = sitemap.urlset.url.filter(
     (url) =>
       url.loc !== `${rootUrl}/tags` &&
@@ -36,7 +104,7 @@ async function trimXML() {
     `Reducing ${sitemap.urlset.url.length} urls to ${filteredUrls.length} urls`
   );
 
-  sitemap.urlset.url = filteredUrls;
+  sitemap.urlset.url = await enrichUrlsWithLastmod(filteredUrls);
 
   const builder = new XMLBuilder({ format: false, ignoreAttributes: false });
   const shorterSitemapXml = builder.build(sitemap);
