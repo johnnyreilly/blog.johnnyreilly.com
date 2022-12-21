70f6f690a1553588d6e4593d1b2eb451fa9b9d85
johnnyreilly <johnny_reilly@hotmail.com>
2022-12-21 13:49:46 +0000

    Deploying to gh-pages from @ johnnyreilly/blog.johnnyreilly.com@2b3cfe21a637cfabb7d803e8baa88b393d39ea2f 🚀

---
+++ assets/js/7b29072b.7b085ee5.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkblog_johnnyreilly_com=self.webpackChunkblog_johnnyreilly_com||[]).push([[14880],{57505:e=>{e.exports=JSON.parse('{"permalink":"/tags/function-syntax","page":1,"postsPerPage":1,"totalPages":1,"totalCount":1,"blogDescription":"The blog of johnnyreilly","blogTitle":"I CAN MAKE THIS WORK"}')}}]);
