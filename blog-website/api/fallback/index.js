93bfc5b3a2960920adfba1becba97ed0b3c5eb30
John Reilly <johnny_reilly@hotmail.com>
2022-12-22 09:16:00 +0000

    feat: function app fallback (#384)
    * feat: function app fallback
    
    * fix: logic
    
    * get deets
    
    * log
    
    * feat: all routes
    
    * 302 not 404
    
    * fear: redirects
    
    * feat: redirect 404
    
    * feat: 302 with url in query
    
    * feat:originalUrl
    
    * feat: logging
    
    * do we need this?
    
    * allow https://gist.github.com

---
+++ blog-website/api/fallback/index.js
@@ -0,0 +1,42 @@
+//@ts-check
+const { parseURL } = require('ufo');
+const routes = require('./redirects');
+
+async function fallback(context, req) {
+  /** @type string */ const originalUrl = req.headers['x-ms-original-url'];
+  if (originalUrl) {
+    // This URL has been proxied as there was no static file matching it.
+    context.log(`x-ms-original-url: ${originalUrl}`);
+
+    const parsedURL = parseURL(originalUrl);
+
+    const matchedRoute = routes.find((route) =>
+      parsedURL.pathname.includes(route.route)
+    );
+
+    if (matchedRoute) {
+      context.log(`Redirecting ${originalUrl} to ${matchedRoute.redirect}`);
+
+      context.res = {
+        status: matchedRoute.statusCode,
+        headers: { location: matchedRoute.redirect },
+      };
+      return;
+    }
+  }
+
+  context.log(
+    `No explicit redirect for ${originalUrl} so will redirect to 404`
+  );
+
+  context.res = {
+    status: 302,
+    headers: {
+      location: originalUrl
+        ? `/404?originalUrl=${encodeURIComponent(originalUrl)}`
+        : '404',
+    },
+  };
+}
+
+module.exports = fallback;
