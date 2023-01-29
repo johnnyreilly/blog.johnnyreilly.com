f2cca976bea86d7ca91de75f7e64817d1f889f70
John Reilly <johnny_reilly@hotmail.com>
2023-01-29 06:10:02 +0000

    feat: JSON LD breadcrumb (#416)
    * feat: JSON LD breadcrumb
    
    * feat: lose blog post

---
+++ blog-website/src/theme/BlogArchivePage/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import BlogArchivePage from '@theme-original/BlogArchivePage';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+
+export default function BlogArchivePageWrapper(props) {
+  const { siteConfig } = useDocusaurusContext();
+
+  // https://developers.google.com/search/docs/appearance/structured-data/breadcrumb#json-ld
+  const breadcrumbStructuredData = {
+    '@context': 'https://schema.org',
+    '@type': 'BreadcrumbList',
+    itemListElement: [
+      {
+        '@type': 'ListItem',
+        position: 1,
+        name: 'Home',
+        item: siteConfig.url,
+      },
+      {
+        '@type': 'ListItem',
+        position: 2,
+        name: 'Archive',
+      },
+    ],
+  };
+
+  return (
+    <>
+      <script type="application/ld+json">
+        {JSON.stringify(breadcrumbStructuredData)}
+      </script>
+      <BlogArchivePage {...props} />
+    </>
+  );
+}
