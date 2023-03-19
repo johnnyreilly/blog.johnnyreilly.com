76899b197f0e58854b10f1b445c7b0b57824d44e
johnnyreilly <johnny_reilly@hotmail.com>
2023-03-19 15:33:25 +0000

    feat: playwright tests

---
+++ blog-website-tests/tests/the.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from '@playwright/test';
+
+test('page should have title and a root navigation link', async ({
+  page,
+  baseURL,
+}) => {
+  await page.goto(baseURL!);
+  const title = await page.title();
+  expect(title).toBe('johnnyreilly | johnnyreilly');
+
+  const navTitle = page.getByRole('link', {
+    name: 'Profile picture of John Reilly John Reilly ❤️🌻',
+  });
+  await expect(navTitle).toBeVisible();
+});
+
+test('can navigate to about page', async ({ page, baseURL }) => {
+  await page.goto(baseURL!);
+  await page.getByRole('link', { name: 'About', exact: true }).click();
+
+  const navTitle = page.getByRole('heading', {
+    name: "Hi! I'm John Reilly - welcome! ❤️🌻",
+  });
+  await expect(navTitle).toBeVisible();
+});
