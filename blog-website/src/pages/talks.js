0ab18a3a7a5907de83ba0e29fd193bccbf20a5fd
johnnyreilly <johnny_reilly@hotmail.com>
2023-03-24 19:59:46 +0000

    feat: talks sprucing up

--- blog-website/src/pages/talks.js
+++ blog-website/src/pages/talks.js
@@ -6,31 +6,38 @@ import styles from './styles.module.css';
 
 const talks = [
   {
+    id: '3W8vCEtRn68?start=220',
     permalink: 'https://www.youtube.com/watch?v=3W8vCEtRn68&t=220s',
     formattedDate: 'February 16th, 2023',
     title: 'LogRocket Meetup: Understanding API data-fetching methods in React',
   },
   {
+    id: 'O58b4r21sz4?start=1735',
     permalink: 'https://www.youtube.com/watch?v=O58b4r21sz4&t=1735s',
     formattedDate: 'January 13th, 2023',
     title: 'TypeScript London: Me and Open Source',
   },
   {
+    id: 'Kl9M7eut5Yo',
     permalink: 'https://youtu.be/Kl9M7eut5Yo',
     formattedDate: 'June 22nd, 2022',
-    title: 'LogRocket Meetup: How to start a blog using Docusaurus, GitHub Actions, and Azure Static Web Apps'    
+    title:
+      'LogRocket Meetup: How to start a blog using Docusaurus, GitHub Actions, and Azure Static Web Apps',
   },
   {
+    id: '5MZoAcheyE4?start=240',
     permalink: 'https://youtu.be/5MZoAcheyE4?t=240',
     formattedDate: 'December 1st, 2021',
     title: 'LogRocket Meetup: TypeScript Alternatives – JSDoc JavaScript',
   },
   {
+    id: 'zi1CHB-eVck?start=282',
     permalink: 'https://youtu.be/zi1CHB-eVck?start=282',
     formattedDate: 'October 27th, 2021',
     title: 'LogRocket Meetup: Structured data, SEO and React',
   },
   {
+    id: 'LxZx3ycrxI0',
     permalink: 'https://youtu.be/LxZx3ycrxI0',
     formattedDate: 'September 30th, 2021',
     title: 'LogRocket Meetup: Write more readable code with TS 4.4',
@@ -49,17 +56,29 @@ function Talks() {
         <section className={styles.features}>
           <div className="container">
             <div className="row">
-              <div className={clsx('col col--12', styles.feature)}>
-                <ul>
-                  {talks.map((talk) => (
-                    <li key={talk.formattedDate}>
-                      <Link to={talk.permalink}>
-                        {talk.formattedDate} - {talk.title}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
+              {talks.map((talk) => (
+                <div className="card-demo padding--md margin--md">
+                  <div className="card">
+                    <div className="card__header">
+                      <h3 style={{ maxWidth: '560px' }}>
+                        <Link to={talk.permalink}>{talk.title}</Link>
+                      </h3>
+                      <h4>{talk.formattedDate}</h4>
+                    </div>
+                    <div className="card__image">
+                      <iframe
+                        width="560"
+                        height="315"
+                        src={`https://www.youtube-nocookie.com/embed/${talk.id}`}
+                        title={talk.title}
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowFullScreen
+                      ></iframe>
+                    </div>
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
         </section>
