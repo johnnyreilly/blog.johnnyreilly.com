ecdb1efafbc10c560e531219a84266913dc1e706
John Reilly <johnny_reilly@hotmail.com>
2023-04-05 19:16:51 +0100

    feat: FAQ structured data (#514)
    * feat: FAQ structured data
    
    * feat: add FAQs component
    
    * feat: 2022->2023
    
    * fix: broken links
    
    * feat: support mdx
    
    * feat: simple api
    
    * feat: name pointless

---
+++ blog-website/src/theme/MDXComponents/FAQStructuredData/types.d.ts
@@ -0,0 +1,23 @@
+export interface FAQ {
+  question: string;
+  answer: string;
+}
+
+export interface FAQStructuredDataProps {
+  faqs: FAQ[];
+}
+
+export interface FAQStructuredData {
+  '@context': string;
+  '@type': string;
+  mainEntity: FAQQuestionStructuredData[];
+}
+
+export interface FAQQuestionStructuredData {
+  '@type': 'Question';
+  name: string;
+  acceptedAnswer: {
+    '@type': 'Answer';
+    text: string;
+  };
+}
