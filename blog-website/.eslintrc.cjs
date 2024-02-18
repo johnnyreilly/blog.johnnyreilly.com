bda145599f6d5bbba6f02382c69e677e29dda757
John Reilly <johnny_reilly@hotmail.com>
2024-02-18 11:32:07 +0000

    feat: no-unused-vars (#843)
    * feat: no-unused-vars
    
    * fix: start
    
    * fix: simpler
    
    * feat: links
    
    * fix: missing nwords
    
    * feat: halfway
    
    * fix: lint output
    
    * feat: last of post
    
    * feat: crush
    
    * fix: nits
    
    * Update index.md

--- blog-website/.eslintrc.cjs
+++ blog-website/.eslintrc.cjs
@@ -15,6 +15,18 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
     '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        args: 'all',
+        argsIgnorePattern: '^_',
+        caughtErrors: 'all',
+        caughtErrorsIgnorePattern: '^_',
+        destructuredArrayIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        ignoreRestSiblings: true,
+      },
+    ],
   },
   settings: {
     react: {
